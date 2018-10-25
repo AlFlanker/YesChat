21c69f01f14305b07b5c6b630a0bcfe625413def
Alex Raspopin <alexflanker89@gmail.com>
2018-10-25 16:33:48 +0300

    Add @Scope("session") to SocketHandler

--- src/main/java/SimpleYesChat/YesChat/config/SocketHandler.java
+++ src/main/java/SimpleYesChat/YesChat/config/SocketHandler.java
@@ -13,13 +13,14 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.BeanFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 import org.springframework.web.socket.*;
 import org.springframework.web.socket.handler.AbstractWebSocketHandler;
 
 import java.io.IOException;
 import java.nio.ByteBuffer;
-
+@Scope("session")
 @Component
 public class SocketHandler extends AbstractWebSocketHandler {
 
